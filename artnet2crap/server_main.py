fc9f384841ebdc8d310ec697f9064ed9c4eb99d1
Uwe Kamper <me@uwekamper.de>
2022-10-09 19:46:52 +0200

    remove separate pixel processing coroutine

--- artnet2crap/server_main.py
+++ artnet2crap/server_main.py
@@ -5,12 +5,6 @@ import sys
 from artnet2crap.artnet_server import ArtNetServerProtocol
 from artnet2crap.crap_client import crap_client_coroutine
 from artnet2crap.framebuffer import framebuffer
-# from artnet2crap.pixel_processing import pixel_processing_coroutine
-
-
-# fmt = "{time} - {name} - {level} - {message}"
-# logger.add("spam.log", level="DEBUG")   #, format=fmt)
-# logger.add(sys.stderr, level="DEBUG")   #, format=fmt)
 
 log = logging.getLogger()
 log.setLevel(logging.INFO)
@@ -21,7 +15,6 @@ formatter = logging.Formatter('%(asctime)s - %(name)s - %(message)s')
 handler.setFormatter(formatter)
 log.addHandler(handler)
 
-packet_queue = asyncio.Queue()
 
 async def create_art_net_coro(frame_buffer):
     loop = asyncio.get_event_loop()
@@ -43,7 +36,6 @@ def main():
     coro = create_art_net_coro(frame_buffer=framebuffer)
     transport, _ = loop.run_until_complete(coro)
     asyncio.ensure_future(crap_client_coroutine(loop, framebuffer))
-    # asyncio.ensure_future(pixel_processing_coroutine(loop, framebuffer, packet_queue))
     loop.run_forever()
 
 
